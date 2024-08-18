d4db19f1cf78f755602d52e9956d56336ea7e2ab
mageOfstructs <jason.puschnig@gmx.at>
2024-08-18 22:33:43 +0200

    add demo fn

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io::stdout;
+
+use crossterm::{
+    cursor::{MoveToColumn, MoveToRow},
+    event, execute,
+    style::Print,
+    terminal::{self, ClearType},
+};
+
+/// crossterm is a set of "low-level", cross-platform APIs that handle terminal output
+/// This could be a potential "frontend", due to it being cross-platform, having *some* way of
+/// async, while still letting us do all the UI design
+fn demo() -> std::io::Result<()> {
+    terminal::enable_raw_mode()?;
+    let (w, h) = terminal::size()?;
+    execute!(
+        stdout(),
+        terminal::Clear(ClearType::All),
+        MoveToRow(h / 2),
+        MoveToColumn(w / 2),
+        Print("Hi from Neoxide!")
+    )?;
+    loop {
+        match event::read()? {
+            event::Event::Key(_event) => {
+                break;
+            }
+            _ => {}
+        }
+    }
+    execute!(stdout(), terminal::Clear(ClearType::All))?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
+
+fn main() {}
