86df5cdb5bf2f92d7c2ce6db04647490b6c0891c
mageOfstructs <jason.puschnig@gmx.at>
2024-08-19 00:32:46 +0200

    add primitive render API

---
+++ src/core/render.rs
@@ -0,0 +1,23 @@
+use std::io::{stdout, Write};
+
+use crossterm::{
+    cursor::MoveTo,
+    queue,
+    style::Print,
+    terminal::{Clear, ClearType},
+};
+
+fn print_text(text: &str, x: u16, y: u16) {
+    queue!(stdout(), MoveTo(x, y), Print(text));
+}
+
+fn print_new_line(text: &str, y: u16) {
+    print_text(text, 0, y)
+}
+
+pub fn print_lines(text: &str, start_row: u16) {
+    text.lines()
+        .enumerate()
+        .for_each(|i, line| print_new_line(line, start_row + i));
+    stdout().flush();
+}
