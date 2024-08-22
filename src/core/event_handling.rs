d9d946dedaa611db258d4b4ec2661e3049d7c285
Vincent Winkler <vincentius.winkler@gmail.com>
2024-08-22 21:27:52 +0200

    event handler is nearly functional

--- src/core/event_handling.rs
+++ src/core/event_handling.rs
@@ -1,85 +1,103 @@
 use rand::Rng;
+use std::future::Future;
 use std::hash::Hash;
-use std::sync::{Arc, Mutex, PoisonError};
+use std::pin::Pin;
+use std::sync::Arc;
 use std::{collections::HashMap, ops::AsyncFn};
 use strum::EnumCount;
 use strum_macros::EnumCount as EnumCountMacro;
 use tokio;
+use tokio::pin;
+use tokio::sync::Mutex;
 
-#[derive(PartialEq, Hash, EnumCountMacro)]
+#[derive(Clone, Copy, PartialEq, Hash, EnumCountMacro)]
 #[repr(u8)]
 enum DemoEvent {
-    InsertEnter(String),
-    NormalEnter(i32),
+    InsertEnter,
+    NormalEnter,
     Quit,
 }
 
-struct EventCallback<E, A>
-where
-    A: AsyncFn(E) -> Result<(), &'static str>,
-{
-    callback: A,
+async fn get_enum_position<T>(enum_type: T) -> u8 {
+    let ptr = &enum_type as *const _ as *const u8;
+    let size = std::mem::size_of_val(&enum_type);
+    let bytes: &[u8] = unsafe { std::slice::from_raw_parts(ptr, size) };
+    bytes[0]
+}
+
+struct DemoEventData {
+    cursor_position: i32,
+    cool_string: String,
+}
+
+pub struct EventCallback<E, D> {
+    callback: Arc<
+        Box<dyn Fn(Arc<Mutex<D>>) -> Pin<Box<dyn Future<Output = ()> + Send + Sync>> + Send + Sync>,
+    >,
     permanent: bool,
     event: E,
 }
 
-pub struct EventHandler<E, A>
+pub struct EventHandler<E, D>
 where
-    E: Eq + Hash + EnumCount,
-    A: AsyncFn(E) -> Result<(), &'static str>,
+    E: EnumCount + Clone + Copy + Send,
+    D: Send,
 {
-    subscriptions: Arc<Mutex<Vec<HashMap<u32, EventCallback<E, A>>>>>,
+    subscriptions: Mutex<Vec<HashMap<u32, EventCallback<E, D>>>>,
 }
 
-impl<E, A> EventHandler<E, A>
+impl<E, D> EventHandler<E, D>
 where
-    E: Eq + Hash + EnumCount,
-    A: AsyncFn(E) -> Result<(), &'static str>,
+    E: EnumCount + Clone + Copy + Send + 'static,
+    D: Send + 'static,
 {
-    async fn new() -> EventHandler<E, A> {
+    pub async fn new() -> EventHandler<E, D> {
         EventHandler {
-            subscriptions: Arc::new(Mutex::new(Vec::with_capacity(E::COUNT))),
+            subscriptions: Mutex::new(Vec::with_capacity(E::COUNT)),
         }
     }
 
-    async fn get_enum_position<T>(enum_type: T) -> u8 {
-        let ptr = &enum_type as *const _ as *const u8;
-        let size = std::mem::size_of_val(&enum_type);
-        let bytes: &[u8] = unsafe { std::slice::from_raw_parts(ptr, size) };
-        bytes[0]
-    }
-
-    async fn subscribe(&self, event: E, callback: EventCallback<E, A>) -> Result<u32, String> {
-        let subscriptions = Arc::clone(&self.subscriptions);
-
-        let mut lock = subscriptions.lock().map_err(|e| e.to_string())?;
+    pub async fn subscribe(&self, event_callback: EventCallback<E, D>) -> u32 {
+        let mut lock = self.subscriptions.lock().await;
         let callback_map = lock
-            .get_mut(Self::get_enum_position(event).await as usize)
-            .expect("unsafe stuff went really wrong");
+            .get_mut(get_enum_position(event_callback.event).await as usize)
+            .expect("unsafe code not so good");
         let id: u32 = rand::thread_rng().gen();
-        callback_map.insert(id, callback);
-        Ok(id)
+        callback_map.insert(id, event_callback);
+        id
     }
 
-    async fn unsubscribe(&mut self, event: E, id: u32) -> Result<(), String> {
-        let subscriptions = Arc::clone(&self.subscriptions);
-        let mut lock = subscriptions.lock().map_err(|e| e.to_string())?;
+    pub async fn dispatch(&self, event: E, data: D)
+    where
+        E: Send + 'static,
+        D: Send + 'static,
+    {
+        let lock = self.subscriptions.lock().await;
+        let data = Arc::new(Mutex::new(data));
         let callback_map = lock
-            .get_mut(Self::get_enum_position(event).await as usize)
-            .expect("unsafe stuff went really wrong");
-        match callback_map.remove(&id) {
-            Some(_) => return Ok(()),
-            None => return Err("No callback with this id found".to_string()),
+            .get(get_enum_position(event).await as usize)
+            .expect("unsafe code not so good");
+        for (_, event_callback) in callback_map {
+            let callback = Arc::clone(&event_callback.callback);
+            let callback_data = Arc::clone(&data);
+            tokio::task::spawn(async move {
+                (callback)(callback_data).await;
+            });
         }
     }
+}
 
-    async fn dispatch(&self, event: E) -> Result<(), String> {
-        let subscriptions = Arc::clone(&self.subscriptions);
-        let mut lock = subscriptions.lock().map_err(|e| e.to_string())?;
-        let callback_map = lock
-            .get_mut(Self::get_enum_position(event).await as usize)
-            .expect("unsafe stuff went really wrong");
-        callback_map.iter().for_each(|event_callback| todo!());
-        Ok(())
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_unsafe_code() {
+        assert_eq!(get_enum_position(DemoEvent::Quit).await, 2);
+    }
+
+    #[tokio::test]
+    async fn test_event_handler() {
+        let event_handler = EventHandler::<DemoEvent,DemoEventData>::new().await;
+        event_handler.subscribe();
     }
 }
