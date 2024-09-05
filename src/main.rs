bcc5bb80e2171bbdf5f7ad63255c8f492ff884d4
mageOfstructs <jason.puschnig@gmx.at>
2024-09-05 09:19:09 +0200

    fix/add demos

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use neoxide::core::io;
+use neoxide::core::editor::motions::{Motion, MotionDirection, UpDownMotion};
+use neoxide::core::event_handling::EventCallback;
+use neoxide::core::{io, logger};
 use std::io::{prelude::*, stdin};
 use std::ops::AddAssign;
 use std::time::Duration;
 use std::{io::stdout, process::Command};
+use tokio::task::JoinHandle;
 
 use crossterm::{
     cursor::{MoveToColumn, MoveToRow},
@@ -37,29 +40,16 @@ fn demo() -> std::io::Result<()> {
     Ok(())
 }
 
-use neoxide::core::render::ClientBuffer;
+use neoxide::core::render::{manager::ANSICode, ClientBuffer};
 async fn demo_render() -> std::io::Result<()> {
-    // terminal::enable_raw_mode()?;
-    // stdout().execute(terminal::Clear(ClearType::All))?;
-    // let mut buf1: Buffer<_> = Buffer::default("buf1");
-    // buf1.children.push("Test");
-    //
-    // let mut buf2: Buffer<_> = Buffer::default("buf2");
-    // buf2.offx = 10;
-    // buf2.offy = 10;
-    // buf2.children.push("Test 2");
-    //
-    // buf1.render().await?; // but our wise friend BUFMAN_SINGLETON now watches over us naive beings
-    //                       // and makes sure we don't overwrite eachother's framebuffer
-    // buf2.render().await?;
-    // terminal::disable_raw_mode()?;
     terminal::enable_raw_mode()?;
-    let buf1 = ClientBuffer::build(0, true).await.unwrap();
+    let mut buf1 = ClientBuffer::build(0, true).await.unwrap();
     buf1.set_content(String::from("Test")).await.unwrap();
-    let buf2 = ClientBuffer::build(0, true).await.unwrap();
+    let mut buf2 = ClientBuffer::build(0, true).await.unwrap();
     buf2.set_content(String::from("Test 2")).await.unwrap();
     buf1.center().await;
-    let buf3 = ClientBuffer::build(0, true).await.unwrap();
+    let mut buf3 = ClientBuffer::build(0, true).await.unwrap();
+    buf1.focus().await.unwrap();
     buf3.set_content(String::from("Test 3")).await.unwrap();
     drop(buf1);
     drop(buf2);
@@ -81,13 +71,64 @@ async fn benchmark(rounds: u32) {
     println!("Avg time per round: {:.3?}", sum.div_f64(rounds.into()));
 }
 
+use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventState, KeyModifiers};
+use neoxide::core::input::{input_loop, subscribe, EvtData, InputConfig, InputEvent};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+// async fn editor_demo() -> JoinHandle<std::io::Result<()>> {
+//     let handle = tokio::spawn(input_loop(InputConfig {
+//         bracketed_paste: false,
+//         focus_change: false,
+//         mouse_capture: false,
+//     }));
+//     let buf = io::open_file("log.neo2").await.unwrap();
+//     subscribe(EventCallback::new(
+//         Arc::new(Box::new(move |evt: Arc<Mutex<_>>| {
+//             let evt = evt.clone();
+//             let buf = buf.clone();
+//             let fut = async move {
+//                 let evt = evt.lock().await;
+//                 if let Event::Key(KeyEvent {
+//                     code,
+//                     modifiers: _,
+//                     kind: _,
+//                     state: _,
+//                 }) = evt.0
+//                 {
+//                     let content = buf.get_content().await;
+//                     if code == KeyCode::Char('j') {
+//                         UpDownMotion.get_new_cursor_position(
+//                             content,
+//                             buf.cursor_position(),
+//                             MotionDirection::Foward,
+//                         );
+//                     }
+//                     logger::log(LogLevel::Debug, format!("Got keycode: {code}").as_str()).await;
+//                 }
+//             };
+//             Box::pin(fut)
+//         })),
+//         true,
+//         InputEvent(Event::Key(KeyEvent::new(
+//             event::KeyCode::Char(' '),
+//             KeyModifiers::empty(),
+//         ))),
+//     ))
+//     .await;
+//     return handle;
+// }
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let _ = Command::new("rm").arg(LOGFILE_PATH).output();
-    let buf = io::open_file("log.neo2").await;
+    terminal::enable_raw_mode()?;
+    demo_render().await.unwrap();
+    terminal::disable_raw_mode()?;
+    // let test = editor_demo().await.await??;
     // bench(10).await;
-    // demo_render().await.unwrap();
     // let mut stdin = stdin();
     // let _ = stdin.read(&mut [0u8]).unwrap();
+    // let buf = io::open_file("log.neo2").await.unwrap();
+    loop {}
     Ok(())
 }
